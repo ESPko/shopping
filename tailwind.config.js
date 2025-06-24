19eb7840cc8a07c8470dadf2d49ad3e0de0cceac
hyun-e-e <sea960928@gmail.com>
2025-06-24 16:07:18 +0900

    0624

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
